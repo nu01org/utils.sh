d405cfa8b57cf2cfac22484cb72b4d816c98e183
Julio Faerman <356476+faermanj@users.noreply.github.com>
2026-06-11 17:53:33 +0200

    wip

--- push_ga_tag.sh
+++ push_ga_tag.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 major="0"
 minor="1"
 build=$(date +%Y%m%d%H%M)
-version="$major.$minor.$build"
+version="$major.$minor.$build-GA"
 
 git tag -a "$version" -m "Version $version"
 git push origin "$version"
